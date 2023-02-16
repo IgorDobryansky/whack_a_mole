9bcdb8429bd4107a4c200f1ac15d479588b7ec8f
IhorDobryanskiy <l48507100120@gmail.com>
2023-02-16 21:50:27 +0200

    beta version

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,79 @@
 import createElement from "./functions/create_element.js";
 
 const app = document.getElementById("app");
+const startButton = document.querySelector(".start");
+const hardMode = document.getElementById("500");
+const normalMode = document.getElementById("1000");
+const easyMode = document.getElementById("1500");
+
+const cells = [...document.querySelectorAll("td")];
+
+let playerPoint = 0;
+let computerPoint = 0;
+
+let cellDataNumber = 0;
+
+cells.forEach((cell) => {
+  cell.setAttribute("data-number", cellDataNumber);
+  cellDataNumber++;
+});
+
+let cellsNumber = cells.map((cell, index) => index);
+
+let timeout;
+
+let interval;
+
+let difficultyMode = 1500;
+
+function getRandomCell(cellActivationTimeout = difficultyMode) {
+  if (computerPoint === 10) {
+    clearInterval(interval);
+    return;
+  }
+
+  if (playerPoint === 10) {
+    clearInterval(interval);
+    return;
+  }
+  const randomCell = Math.floor(Math.random() * cellsNumber.length);
+
+  cells.map((cell) => {
+    if (+cell.getAttribute("data-number") === cellsNumber[randomCell]) {
+      cell.addEventListener("click", playerGetPoint);
+
+      cell.style.background = "blue";
+
+      timeout = setTimeout(() => {
+        cell.style.background = "red";
+        cell.removeEventListener("click", playerGetPoint);
+        computerPoint++;
+        if (computerPoint === 10) {
+          clearInterval(interval);
+          return;
+        }
+        cellsNumber = [...cellsNumber].filter(
+          (cell, index) => index !== randomCell
+        );
+      }, difficultyMode);
+
+      function playerGetPoint() {
+        cell.style.background = "green";
+        playerPoint++;
+        clearTimeout(timeout);
+        cellsNumber = [...cellsNumber].filter(
+          (cell, index) => index !== randomCell
+        );
+      }
+    }
+  });
+}
+
+startButton.addEventListener("click", () => {
+  [...document.querySelectorAll("input")].forEach((input) => {
+    if (input.checked) {
+      difficultyMode = +input.value;
+      interval = setInterval(getRandomCell, 2000);
+    }
+  });
+});
